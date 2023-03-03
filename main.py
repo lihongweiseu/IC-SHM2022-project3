c0ed5725732769827b322da7bd063503bf775456
Your Name <shuohaopolyu@gmail.com>
2023-03-03 21:34:46 +0800

    add oma functions

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # draw figure for matrix plot
-exec(open("mtx_nrmse.py").read())
+# exec(open("mtx_nrmse.py").read())
 
 # make predictions on the first natural frequencies for damaged cases
-exec(open("freq_pred.py").read())
+# exec(open("freq_pred.py").read())
+
+# operational modal analysis
+exec(open("oma.py").read())
