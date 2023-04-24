8829960c33604e6afb2a54a664cc1a135c74c28f
Shuo HAO <shuohaopolyu@gmail.com>
2023-04-24 22:18:11 +0800

    add calculation for fig4

--- Task 2 damage identification/project3_damage_task_code/figs_and_tables.py
+++ Task 2 damage identification/project3_damage_task_code/figs_and_tables.py
@@ -14,7 +14,8 @@ import scipy.io as io
 # rho = np.array([1, 5.6, 4.2, 2.1, 3.0, 1.2, 3.4, 0.3, 1.5, 0.4])*1e4
 # E = np.array([2, 3.4, 0.2, 0.8, 1.2, 4.2, 5.3, 6.6, 2.4, 0.5])*1e10
 # I = np.array([8.33, 6.67, 8.00, 9.17, 4.00, 5, 3.33, 3.00, 4.33, 5.17])*1e-2
-# alphas_set = np.array([[0.0, 0.2, 0.0],
+# alphas_set = np.array([[0.0, 0.0, 0.0],
+#                        [0.0, 0.2, 0.0],
 #                        [0.0, 0.3, 0.0],
 #                        [0.0, 0.4, 0.0],
 #                        [0.1, 0.0, 0.0],
@@ -27,11 +28,14 @@ import scipy.io as io
 #                        ])
 # beam = beam_fem(rho=rho[0], E=E[0], A=A[0], I=I[0], L=0.5)
 # ratio = beam.freqs_ratio()
-# pred_freq = []
-# for i in range(10):
+# freq = []
+# for i in range(11):
 #     model_freq = beam.frequency(order=1, alphas=alphas_set[i, :])
-#     pred_freq.append(round(model_freq*ratio, 4))
-# print(pred_freq)
+#     freq.append(round(model_freq, 4))
+# freq_ans = np.array([9.4367, 9.3613, 9.3093, 9.2421, 9.4251,
+#                     9.3924, 9.3345, 9.3356, 9.2167, 9.2933, 9.1751])
+# print(np.array(freq)/freq_ans)
+# print(freq)
 
 
 ### Figure: Computed -log10(NRMSE) between each two different first mode shapes ###
